8bec6008becb5ad5b91b075c2268d273d8348360
CnuJimin <194115@jnu.ac.kr>
2025-01-02 21:18:13 +0900

    피보나치 함수/정답 확인 후 다시 품 / 25M

--- src/week5/Boj_1003.java
+++ src/week5/Boj_1003.java
@@ -1,4 +1,4 @@
-package Week5;
+package week5;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,34 +19,39 @@ public class Boj_1003 {
     * DP는 한번 사용한 값을 저장해서 사용하기 때문에 배열을 이용해서 값을 저장해두어야 함
     * */
 
-    static int N ;
-    static int zero ;
-    static int one;
+    static Integer[][] dp = new Integer[41][2]; // N의 범위가 0~40
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
 
-        for (int i = 0; i < N; i++) {
-            fibo(Integer.parseInt(br.readLine()));
-            sb.append(zero).append(" ").append(one).append("\n");
 
-            zero = 0;
-            one = 0;
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            fibo(N);
+            sb.append(dp[N][0] + " " + dp[N][1]).append("\n");
+
         }
 
         System.out.println(sb);
     }
 
-    public static int fibo(int n) {
-        if (n == 1) {
-            return one ++;
-        } else if (n == 0) {
-            return zero++;
-        } else {
-            return fibo(n - 1) + fibo(n - 2);
+    public static Integer[] fibo(int N) {
+        if (dp[N][0] == null || dp[N][1] == null) {
+            dp[N][0] = fibo(N - 1)[0] + fibo(N - 2)[0];
+            dp[N][1] = fibo(N - 1)[1] + fibo(N - 2)[1];
         }
 
+        return dp[N];
+
     }
+
+
 }
