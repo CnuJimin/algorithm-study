c9c7d248d88b5569e72088f35944af9a09adfcce
CnuJimin <194115@jnu.ac.kr>
2025-01-05 15:21:14 +0900

    1만들기 / 30M/ 오답 + 풀이 추가

--- src/week5/Boj_1463.java
+++ src/week5/Boj_1463.java
@@ -8,15 +8,14 @@ public class Boj_1463 {
     //1로 만들기
     /*
      * if N % 2 = 0 => dp[N%2] + 1
-    * if N % 3 = 0 => dp[N%3] + 1
-    * else dp[N-1] + 1
-    * 틀렸음 주어진 연산 순서가 우선순위가 아닌 것 같음
-    * 2와 3으로 나눴을 떄 그리고 -1 중 더 적은 값을 선택하도록 해야 함
-    * => 이렇게 하면 시간 초과
-    * for 문으로 만들어보기 -> 정답
-    * */
+     * if N % 3 = 0 => dp[N%3] + 1
+     * else dp[N-1] + 1
+     * 틀렸음 주어진 연산 순서가 우선순위가 아닌 것 같음
+     * 2와 3으로 나눴을 떄 그리고 -1 중 더 적은 값을 선택하도록 해야 함
+     * => 이렇게 하면 시간 초과 -> 근데 이렇게 할 때 Math.min()에 makeOne(N/3)을 먼저 넣으면 시간 초과가 안뜸
+     * for 문으로 만들어보기 -> 정답
+     * */
     static Long[] dp = new Long[10_000_000];
-/*
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,22 +31,23 @@ public class Boj_1463 {
 
     private static Long makeOne(int N) {
         if (dp[N] == null) {
-            long result = makeOne(N - 1) + 1;
-
-            if (N % 3 == 0) {
-                result = Math.min(result, makeOne(N / 3) + 1);
+            if (N % 6 == 0) {
+                dp[N] = Math.min(makeOne(N - 1), Math.min(makeOne(N / 3), makeOne(N / 2))) + 1;
             }
-
-            if (N % 2 == 0) {
-                result = Math.min(result, makeOne(N / 2) + 1);
+            else if (N % 3 == 0) {
+                dp[N] = Math.min(makeOne(N / 3), makeOne(N - 1)) + 1;
             }
-
-            dp[N] = result;
+            else if (N % 2 == 0) {
+                dp[N] = Math.min(makeOne(N / 2), makeOne(N - 1)) + 1;
+            }
+            else {
+                dp[N] = makeOne(N - 1) + 1;
+            }
         }
         return dp[N];
-
-    => 시간 초과가 뜸
-    }*/
+    }
+}
+/*
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -70,3 +70,4 @@ public class Boj_1463 {
         System.out.println(dp[N]);
     }
 }
+*/
