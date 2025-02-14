ff4344aa9d2107bf55c8460bf87265eb21a9b4a8
CnuJimin <194115@jnu.ac.kr>
2025-02-14 17:52:29 +0900

    신입 사원/ 15M / 시간초과

--- src/week9/Boj_1946.java
+++ src/week9/Boj_1946.java
@@ -8,7 +8,7 @@ public class Boj_1946 {
     /*
     * int[N][2] 배열에 지원자들의 점수를 담음
     * 반복문을 돌면서 만약 지원자의 점수가 다른 지원자의 점수보다 적다면 N --;
-    *
+    * -> 이렇게 풀면 시간초과
     *
     * */
 
@@ -30,14 +30,20 @@ public class Boj_1946 {
                 }
             }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (array[i][0] < array[j][0] && array[i][1] < array[j][1]) {
-                        N --;
-                    }
+            Arrays.sort(array, Comparator.comparing(a -> a[0])); // 서류 성적을 기준으로 오름차순 정렬
+
+            int count = 1;
+
+            int minInterview = array[0][1];
+
+            for (int i = 1; i < N; i++) {
+                if (array[i][1] < minInterview) {// 서류 성적을 기준으로 이미 이미 정렬돼 있어서 면접 점수도 낮으면 무조건 탈락
+                    count ++ ;
+                    minInterview = array[i][1];  // 바로 직전 사람이 아니라 지금까지의 최고 경쟁자를 이겨야 합격할 수 있음
                 }
             }
-            sb.append(N).append("\n");
+            sb.append(count).append("\n");
+
         }
         System.out.println(sb);
     }
